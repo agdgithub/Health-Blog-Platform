926b97cb9eab9c68839cd096b84119cf7aadce62
agdgithub <iit2020182@iiita.ac.in>
2024-05-12 01:06:17 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-
-mkvirtualenv --python=/usr/bin/python3.10 venv
-
-pip install -r requirements.txt
-
-python manage.py collectstatic
+pip install -r requirements.txt 
+python3.12 manage.py collectstatic
