c7a1718c080ebec752a957a39fc222da49f834a2
agdgithub <iit2020182@iiita.ac.in>
2024-05-12 01:01:06 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Install Python 3.10
-sudo apt update
-sudo apt install python3.10
+mkvirtualenv --python=/usr/bin/python3.10 venv
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Run collectstatic
-python3.10 manage.py collectstatic
+python manage.py collectstatic
