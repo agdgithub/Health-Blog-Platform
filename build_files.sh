a67922e60b814aa99e5d77f697c6c39df1c169a0
agdgithub <iit2020182@iiita.ac.in>
2024-05-12 00:51:41 +0530

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
-pip3 install -r requirements.txt 
-python manage.py collectstatic
+#!/bin/bash
+
+# Install Python 3.10
+sudo apt update
+sudo apt install python3.10
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run collectstatic
+python3.10 manage.py collectstatic
